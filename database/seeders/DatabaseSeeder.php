f727de735024ee392b02dcf3c42ff86a3c42c2de
Hanis siddiq <hanissiddiq10@gmail.com>
2025-09-22 09:19:14 +0700

    Upload wowdash-admin template & Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Str;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,31 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+         User::factory()->create([
+            'name' => 'Administrator Simascut',
+            'email' => 'admin@simascut.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('mantap'), // passwordnya : mantap
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Kepala Kantor',
+            'email' => 'kakan@simascut.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('mantap'), // passwordnya : mantap
+            'remember_token' => Str::random(10),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Kepala Seksi',
+            'email' => 'kasi@simascut.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('mantap'), // passwordnya : mantap
+            'remember_token' => Str::random(10),
+        ]);
+
 
         User::factory()->create([
             'name' => 'Test User',
